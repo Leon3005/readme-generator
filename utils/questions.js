5da0b9a3dc17564ff13aaffcce162ee7b3813ab0
Leon <leonwheeler08@gmail.com>
2021-05-02 11:43:30 +0100

    Moved questions to a different JS file

---
+++ utils/questions.js
@@ -0,0 +1,51 @@
+// TODO: Create an array of questions for user input
+const questions = [
+  {
+    type: "input",
+    message: "What is the title of your Application?",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "Enter a label for your badge:",
+    name: "badgeLabel",
+  },
+  {
+    type: "input",
+    message: "Enter a message for your badge:",
+    name: "badgePackage",
+  },
+  {
+    type: "list",
+    message: "Choose a colour for your badge:",
+    name: "badgeColour",
+    choices: ["Red", "Green", "Blue"],
+  },
+  {
+    type: "input",
+    message: "Give a short description of your app:",
+    name: "description",
+  },
+  {
+    type: "input",
+    message: "How will users install your app?",
+    name: "install",
+  },
+  {
+    type: "input",
+    message: "How will users use your app?",
+    name: "usage",
+  },
+  {
+    type: "input",
+    message: "Enter the features of your app:",
+    name: "features",
+  },
+  {
+    type: "input",
+    message: "Enter the file path of a screenshot of the app:",
+    name: "screenshot",
+  },
+];
+
+module.exports = questions;
