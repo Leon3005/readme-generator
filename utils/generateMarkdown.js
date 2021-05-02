477b92414cfb060ade84093e2e68a4969bd6b783
Leon <leonwheeler08@gmail.com>
2021-05-02 11:36:55 +0100

    Aliasing

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -14,20 +14,31 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  const {
+    title,
+    badgeLabel,
+    badgePackage,
+    badgeColour,
+    description,
+    install,
+    usage,
+    features,
+    screenshot,
+  } = data;
   return `
-# ${data.title}
-![Badge](https://img.shields.io/static/v1?label=${data.badgeLabel}&message=${data.badgePackage}&color=${data.badgeColour})
+# ${title}
+![Badge](https://img.shields.io/static/v1?label=${badgeLabel}&message=${badgePackage}&color=${badgeColour})
 ## Description
-${data.description}
+${description}
 ## Table of Contents (Optional)
 - [Installation](#installation)
 - [Usage](#usage)
 - [Credits](#credits)
 - [License](#license)
 ## Installation
-${data.install}
+${install}
 ## Usage
-${data.usage}
+${usage}
 ## License
 The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
 ---
@@ -36,11 +47,11 @@ The last section of a high-quality README file is the license. This lets other d
 ![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
 Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
 ## Features
-${data.features}
+${features}
 ## Screenshots
-![Screenshot](${data.screenshot})
+![Screenshot](${screenshot})
 ## License
-![License](https://img.shields.io/npm/l/${data.badgePackage})
+![License](https://img.shields.io/npm/l/${badgePackage})
 ## Credits
 ## Tests
 `;
